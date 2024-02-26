4de3a8087b54818ddfa74e30d847f2ae2cac32e4
Thomas Restrepo <thomasrestrepoorrego@gmail.com>
2024-02-26 01:41:23 -0500

    Added CORS Global config

--- mental_health_back/api-gateway/src/main/java/co/com/mentalhealth/apigateway/ApiGatewayApplication.java
+++ mental_health_back/api-gateway/src/main/java/co/com/mentalhealth/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,26 @@ package co.com.mentalhealth.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+				//Cambiar el localhost por la variable de entorno con el dominio admitido
+			}
+		};
 	}
 
 }
